70869ea3b7898adc2256f4bef7733a66f6f8ce40
vijayvigneshselvaraj <vijayvigneshselvaraj@gmail.com>
2020-03-31 16:37:33 +0200

    Refactor: Added comments to the sayNumbers method

--- src/main/java/com/bnppf/kata/FizzBuzz.java
+++ src/main/java/com/bnppf/kata/FizzBuzz.java
@@ -18,9 +18,13 @@ public class FizzBuzz {
     public ArrayList<String> sayNumbers() {
         numbers = new ArrayList();
         for (int i=this.fromNumber; i<=this.toNumber; i++) {
+            // Checking divisibility of both number 3 and 5
             if (i%3==0 && i%5==0) numbers.add("FizzBuzz");
+            // Checking divisibility of both number 3
             else if(i%3==0) numbers.add("Fizz");
+            // Checking divisibility of both number 5
             else if(i%5==0) numbers.add("Buzz");
+            // Checking divisibility of not number 3 and 5
             else numbers.add(""+i);
         }
         return numbers;
