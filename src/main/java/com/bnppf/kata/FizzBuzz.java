af7991b71db30cb58db3702a84fcd2f0cb167650
vijayvigneshselvaraj <vijayvigneshselvaraj@gmail.com>
2020-03-31 15:56:23 +0200

    Implementation: Added basic code in FizzBuzz main and test file

--- src/main/java/com/bnppf/kata/FizzBuzz.java
+++ src/main/java/com/bnppf/kata/FizzBuzz.java
@@ -1,5 +1,23 @@
 package com.bnppf.kata;
 
-public class FizzBuzz {
+import java.util.ArrayList;
 
+public class FizzBuzz {
+    // List to hold the final numbers
+    private ArrayList<String> numbers = null;
+
+    // Numbers start and end range (Note: Start and End numbers are inclusive)
+    private int fromNumber;
+    private int toNumber;
+
+    FizzBuzz(int fromNumber, int toNumber) {
+        this.fromNumber = fromNumber;
+        this.toNumber = toNumber;
+    }
+
+    public ArrayList<String> sayNumbers() {
+        numbers = new ArrayList();
+
+        return  numbers;
+    }
 }
--- src/test/java/com/bnppf/kata/FizzBuzzTest.java
+++ src/test/java/com/bnppf/kata/FizzBuzzTest.java
@@ -1,5 +1,19 @@
 package com.bnppf.kata;
 
-public class FizzBuzzTest {
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
+public class FizzBuzzTest {
+    private static FizzBuzz fizzBuzz = null;
+
+    @BeforeClass
+    public static void setUp() {
+        fizzBuzz = new FizzBuzz(1, 100);
+    }
+
+    @Test
+    public void fizzBuzzObjectNotNullCheck() {
+        Assert.assertNotNull(fizzBuzz);
+    }
 }
